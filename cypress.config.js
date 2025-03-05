acccfa8ec50860914a218f0d0fa905d3bbb15cc6
graiche9 <fazia.graiche9@gmail.com>
2025-03-05 15:07:48 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,3 @@
-/*const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'results/my-test-output-[hash].xml',
-  },
-  e2e: {
-    setupNodeEvents(on, config) {
-      //require('cypress-mochawesome-reporter/plugin')(on);
-    },
-  },
-
-});*/
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
