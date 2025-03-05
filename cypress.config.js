ab471680b0c4c13febec03bac6d662ffcd848e8e
graiche9 <fazia.graiche9@gmail.com>
2025-03-05 13:37:09 +0100

    mise à jour du fichier jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output-[hash].xml',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+
     },
   },
 
